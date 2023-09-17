7f01db64164f19d0ac533814397e3ed450b22d5e
Kandeepan Yathuraj <50902529+yathu1@users.noreply.github.com>
2023-09-18 00:24:59 +0530

    Email verification

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
-const { create } = require('../controllers/user,js');
+const { create,verifyEmail,resendEmailVerificationToken } = require('../controllers/user.js');
 const { userValidator, validate } = require('../middlewares/validator');
+
 
 const router = express.Router()
 
 router.post('/create',userValidator,validate,create);
+router.post('/verify-email',verifyEmail);
+router.post('/resend-email-verification-token',resendEmailVerificationToken);
 
 module.exports = router; 
